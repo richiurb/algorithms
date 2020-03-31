43500d201b9668caf98f13095f577aa756038db1
Rikhard Urbanovich <urihard@mail.ru>
2020-03-31 11:14:52 +0300

    add lab6 in M3139 algorithms

---
+++ M3139/lab6 - hashing/6A_Set.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+const int e = 1e6;
+
+vector<vector<int> > vec(e);
+
+int main() {
+	freopen("set.in", "r", stdin);
+	freopen("set.out", "w", stdout);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string operation;
+	while (cin >> operation) {
+		int num;
+		cin >> num;
+		int hash_func = (abs(num * 31 * 31 + num * 101 + 37)) % e;
+
+		if (operation == "insert") {
+			bool flag = true;
+			if (!vec[hash_func].empty()) {
+				for (int i = 0; i < (int)vec[hash_func].size(); i++) {
+					if (vec[hash_func][i] == num) {
+						flag = false;
+						break;
+					}
+				}
+			}
+			if (flag) {
+				vec[hash_func].push_back(num);
+			}
+		}
+
+		if (operation == "delete") {
+			bool flag = false;
+			if (!vec[hash_func].empty()) {
+				for (int i = 0; i < (int)vec[hash_func].size(); i++) {
+					if (flag) {
+						vec[hash_func][i - 1] = vec[hash_func][i];
+					}
+					if (vec[hash_func][i] == num) {
+						flag = true;
+					}
+				}
+				if (flag) {
+					vec[hash_func].pop_back();
+				}
+			}
+		}
+
+		if (operation == "exists") {
+			if (vec[hash_func].empty()) {
+				cout << "false\n";
+			} else {
+				bool flag = false;
+				for (int i = 0; i < (int)vec[hash_func].size(); i++) {
+					if (vec[hash_func][i] == num) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					cout << "true\n";
+				} else {
+					cout << "false\n";
+				}
+			}
+		}
+	}
+}
---
+++ M3139/lab6 - hashing/6B_Map.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+const int e = 1e6;
+
+vector<vector<pair<string, string> > > vec(e);
+
+int main() {
+	freopen("map.in", "r", stdin);
+	freopen("map.out", "w", stdout);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string operation;
+	while (cin >> operation) {
+		string num;
+		cin >> num;
+		int hash_func_l = 0;
+		for (int i = 0; i < min((int)num.size(), 10); i++) {
+			hash_func_l = (hash_func_l +
+				(abs(num[(int)num.size() - i - 1] * 31 * 31 + (int)num[i] * 101))) % e;
+		}
+		int hash_func = (int)(hash_func_l % e);
+
+		if (operation == "put") {
+			string s;
+			cin >> s;
+			bool flag = true;
+			if (!vec[hash_func].empty()) {
+				for (int i = 0; i < (int)vec[hash_func].size(); i++) {
+					if (vec[hash_func][i].first == num) {
+						vec[hash_func][i].second = s;
+						flag = false;
+						break;
+					}
+				}
+			}
+			if (flag) {
+				vec[hash_func].push_back(make_pair(num, s));
+			}
+		}
+
+		if (operation == "delete") {
+			bool flag = false;
+			int a = 0;
+			if (!vec[hash_func].empty()) {
+				for (int i = 0; i < (int)vec[hash_func].size(); i++) {
+					if (vec[hash_func][i].first == num) {
+						a = i;
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					vec[hash_func][a] = vec[hash_func][(int)vec[hash_func].size() - 1];
+					vec[hash_func].pop_back();
+				}
+			}
+		}
+
+		if (operation == "get") {
+			if (vec[hash_func].empty()) {
+				cout << "none\n";
+			} else {
+				bool flag = false;
+				for (int i = 0; i < (int)vec[hash_func].size(); i++) {
+					if (vec[hash_func][i].first == num) {
+						cout << vec[hash_func][i].second << "\n";
+						flag = true;
+						break;
+					}
+				}
+				if (!flag) {
+					cout << "none\n";
+				}
+			}
+		}
+	}
+}
